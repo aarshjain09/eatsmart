820315faca62474d84c064c81b99eb6bc938d53e
Mohit Kumar Jain <mohit198@gmail.com>
2026-07-29 14:37:02 +0530

    Update products.tsx

--- src/routes/products.tsx
+++ src/routes/products.tsx
@@ -6,31 +6,100 @@ export const Route = createFileRoute("/products")({
 });
 
 const products = [
-  {
+ {
     id: 1,
-    image: "https://m.media-amazon.com/images/I/61mDvrFuiQL._SY300_SX300_QL70_FMwebp_.jpg",
-    amazonLink: "https://www.amazon.in/True-Elements-Rolled-Gluten-Wholegrain/dp/B07SBMKVRV?crid=1MCR9PVL75VA1&dib=eyJ2IjoiMSJ9.HFMR1eIREUJ9ry2HSJ-YysAU9EnmxY9ATWcVwSzi0zwv6n0hX4n4057U-QqH9bs2yLLXTQvTpwXmpvte-R-KSXCOFWcMumdra4dP6iv8zv_AbiSCTp0aL6Oq2bg1aDMvWXWuzXsv6i1XyRQ3PtRLIxI0yfm_YGPnrQ8skp9M_dkOqscnqARSDbZaImPpxHoXKKO7rAwpPYy0ZC41r2HzrjnE_d_cr1PHiPYWQmraxANOtlT_esLVN26B3qYfIUZ3cO5OxoEjNgusvIfe6ZbwsJNSRRDRCWSA-BzPWUxZdFs.F4xmoJEuyZRlWnG02dAwW5jfCPmhMZeavmqmdSdA9dU&dib_tag=se&keywords=true%2Belements&nsdOptOutParam=true&qid=1784108411&sbo=m6DjfpMzMLDmL8pSMKX8hw%3D%3D&sprefix=true%2Belements%2B%2Caps%2C255&sr=8-5&th=1&linkCode=ll2&tag=eatsmart-21&linkId=2f3f95a22c5c5f81967d3a2b51d1e930&ref_=as_li_ss_tl",
+    image: “https://m.media-amazon.com/images/I/71620dZ5esL._SL1500_.jpg”,
+    amazonLink:  ”https://link.amazon/B0hEK59SB”,
   },
-  {
+
+{
     id: 2,
-    image: "https://m.media-amazon.com/images/I/61hKwTsa-GL._SX679_.jpg",
-    amazonLink: "https://www.amazon.in/True-Elements-Chia-Seeds-250g/dp/B01M6ZAIXL?crid=1ZSKE9VP8C7LX&dib=eyJ2IjoiMSJ9.Q1bQkNAROLJT9i2aNeMhUcAU9EnmxY9ATWcVwSzi0zwv6n0hX4n4057U-QqH9bs2U0222KkB4ZhpWEiBGZ0rYQmRbZEwzx3wQk5t7Hzto4kVUv90ZX7pXJtWRTyl6lV14fh5cj18Wk_0bJxhD5Tx-D8f4TB7KQpTSokKyw8Xue6BL0Estu2eXYQDwv_YXR8xSOT35-gOOob8yJtL-bgChViIoeMa0pFRyCZXaVjf-EkVUpsE2FZdog0NLKzaLxGOpBigQFpexLa36CW7xoixBcAjj4-LbWepB7QXBiRO-mY.B6XP_aVU_dAThuZJPXC5TTS0jkRTGNxTSYTXDOkAa5o&dib_tag=se&keywords=true%2Belements%2Bseeds&nsdOptOutParam=true&qid=1784108553&sprefix=true%2Belements%2Bseed%2Caps%2C279&sr=8-2-spons&aref=RJujtQ2pCM&sp_csd=d2lkZ2V0TmFtZT1zcF9hdGY&th=1&linkCode=ll2&tag=eatsmart-21&linkId=fd45c5835b98c4dd8492795129e35c53&ref_=as_li_ss_tl",
+    image: ”https://m.media-amazon.com/images/I/716WM50lCNL._SL1500_.jpg”,
+    amazonLink:  ”https://link.amazon/B0dWmbqD1”,
   },
-  {
+
+
+{
     id: 3,
-    image: "https://m.media-amazon.com/images/I/41q3jP1WX5L._SY300_SX300_QL70_FMwebp_.jpg",
-    amazonLink: "https://www.amazon.in/True-Elements-Flax-Seeds-500g/dp/B06XW6F9QG?crid=1ZSKE9VP8C7LX&dib=eyJ2IjoiMSJ9.Q1bQkNAROLJT9i2aNeMhUcAU9EnmxY9ATWcVwSzi0zwv6n0hX4n4057U-QqH9bs2U0222KkB4ZhpWEiBGZ0rYQmRbZEwzx3wQk5t7Hzto4kVUv90ZX7pXJtWRTyl6lV14fh5cj18Wk_0bJxhD5Tx-D8f4TB7KQpTSokKyw8Xue6BL0Estu2eXYQDwv_YXR8xSOT35-gOOob8yJtL-bgChViIoeMa0pFRyCZXaVjf-EkVUpsE2FZdog0NLKzaLxGOpBigQFpexLa36CW7xoixBcAjj4-LbWepB7QXBiRO-mY.B6XP_aVU_dAThuZJPXC5TTS0jkRTGNxTSYTXDOkAa5o&dib_tag=se&keywords=true%2Belements%2Bseeds&nsdOptOutParam=true&qid=1784108553&sbo=m6DjfpMzMLDmL8pSMKX8hw%3D%3D&sprefix=true%2Belements%2Bseed%2Caps%2C279&sr=8-6&th=1&linkCode=ll2&tag=eatsmart-21&linkId=917300884923a787ff4a117c4640f7cd&ref_=as_li_ss_tl",
+    image: ”https://m.media-amazon.com/images/I/51yl3YN07kL._SL1080_.jpg”,
+    amazonLink:  ”https://link.amazon/B0bTWveHW,
   },
-  {
+
+
+
+{
     id: 4,
-    image: "https://m.media-amazon.com/images/I/71Y5PGUmk8L._SL1500_.jpg",
-    amazonLink: "https://link.amazon/B06GeKday",
+    image: ”https://m.media-amazon.com/images/I/71aFLaxhsbL._SL1500_.jpg”,
+    amazonLink: ”https://link.amazon/B01ToIHMb” ,
   },
-  {
-    id: 4,
-    image: "https://m.media-amazon.com/images/I/71MI2MQrHzL._SL1500_.jpg",
-    amazonLink: "https://link.amazon/B05QQNLh1",
+
+
+{
+    id: 5,
+    image: ”https://m.media-amazon.com/images/I/61ncmjQk0cL._SX679_.jpg,
+    amazonLink: ”https://link.amazon/B0bBToXO2” ,
   },
+
+
+
+{
+    id: 6,
+    image: ”https://link.amazon/B02296OVj”,
+    amazonLink:  ”https://m.media-amazon.com/images/I/61mDvrFuiQL.jpg”,
+  },
+
+
+
+{
+    id: 7,
+    image: ”https://m.media-amazon.com/images/I/711JJsEMUPL._SL1500_.jpg”,
+    amazonLink:  ”https://link.amazon/B02A9BpLY”,
+  },
+
+
+{
+    id: 8,
+    image: ”https://m.media-amazon.com/images/I/61dMAFq28zL.jpg”,
+    amazonLink: ”https://link.amazon/B05lhHNhP” ,
+  },
+
+
+{
+    id: 9,
+    image: ”https://m.media-amazon.com/images/I/61hKwTsa-GL._SL1080_.jpg”,
+    amazonLink: ”https://link.amazon/B0bi5T2JY” ,
+  },
+
+
+{
+    id: 10,
+    image: ”https://m.media-amazon.com/images/I/71MfgJ+Kw+L._SL1500_.jpg”,
+    amazonLink:  ”https://link.amazon/B0ffnk7Ux”,
+  },
+
+
+
+{
+    id: 11,
+    image: ”https://m.media-amazon.com/images/I/71TDNgqLfkL._SL1500_.jpg”,
+    amazonLink: ”https://link.amazon/B05UilOnH” ,
+  },
+	
+
+
+{
+    id: 12,
+    image: ”https://m.media-amazon.com/images/I/71TDNgqLfkL._SL1500_.jpg”,
+    amazonLink:  ”https://link.amazon/B0exZMKDe”,
+  },
+
+
+{
+    id: 13,
+    image: ”https://m.media-amazon.com/images/I/71mYr24r8cL._SL1500_.jpg”,
+    amazonLink:  ”https://link.amazon/B028keKpn”,
+  },
+
 
 ];
 
